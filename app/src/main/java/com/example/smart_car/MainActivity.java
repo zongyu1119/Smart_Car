7b2c2b954263236ed657a83121b85b1c1d39c381
zongyu1119 <wuguoqiang0927@hotmail.com>
2020-03-02 13:38:30 +0800

    修改了主页

--- app/src/main/java/com/example/smart_car/MainActivity.java
+++ app/src/main/java/com/example/smart_car/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.smart_car;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button btn_car=(Button)findViewById(R.id.btn_car);
+        btn_car.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView text_state=(TextView)findViewById(R.id.text_state);
+                text_state.setText("点击了小车按钮！");
+            }
+        });
+        
     }
 }
